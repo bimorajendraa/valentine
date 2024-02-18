68e51534095762502c8f0331522525b0c48d1f47
bimorajendraa <elitamelati08@gmail.com>
2024-02-18 23:18:02 +0700

    update button color

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ onClick }) {
   return (
     <button
-      className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg transition-all duration-300"
+      className="py-2 px-4 bg-[#FF7D7D] text-white font-semibold rounded-lg transition-all duration-300"
       onClick={onClick}
     >
       NO
